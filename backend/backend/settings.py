9f2678de021ef4ae01cf115975cd07f2c6275cfe
Rytxxx <k.ryuto.5@icould.com>
2025-02-13 20:46:21 +0900

    fixed-files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w!u3#=nfm_57i%$$wo3mov13a6g178!@!130)+d3h4j&0fn!2x"
+SECRET_KEY = "django-insecure-7__46a#0ib1p+g#k@#glg++jll8l$ym7t^^ob@eo#2e3_xvuq-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "backend.backend.urls"
+ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "backend.backend.wsgi.application"
+WSGI_APPLICATION = "backend.wsgi.application"
 
 
 # Database
